b2869571b7c7cef10ebe34f37fd9f63081f5dec5
kainat <kainatkhan892014@gmail.com>
2024-02-20 19:34:04 -0400

    remove unused imports

--- src/main/java/com/kainat/learnspringbootwebservices/api/dtos/CourseModuleDTO.java
+++ src/main/java/com/kainat/learnspringbootwebservices/api/dtos/CourseModuleDTO.java
@@ -6,7 +6,6 @@ import com.kainat.learnspringbootwebservices.api.validators.ValidIcon;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.hibernate.annotations.NotFound;
 
 import java.util.List;
 
