54fe5dcb31e440c7a85c3019f7ec48a6c5a34856
kainat <kainatkhan892014@gmail.com>
2024-02-20 20:47:51 -0400

    create tutorial api

--- src/main/java/com/kainat/learnspringbootwebservices/api/controllers/TutorialController.java
+++ src/main/java/com/kainat/learnspringbootwebservices/api/controllers/TutorialController.java
@@ -1,4 +1,45 @@
 package com.kainat.learnspringbootwebservices.api.controllers;
 
+import com.kainat.learnspringbootwebservices.api.dtos.TutorialDTO;
+import com.kainat.learnspringbootwebservices.api.services.tutorial.TutorialService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/tutorials")
 public class TutorialController {
+    private final TutorialService tutorialService;
+    @Autowired
+    public TutorialController(TutorialService tutorialService) {
+        this.tutorialService = tutorialService;
+    }
+
+    @GetMapping("module-tutorials/{moduleId}")
+    public ResponseEntity<List<TutorialDTO>> getTutorials(@PathVariable Integer moduleId) {
+       List<TutorialDTO> tutorialDTOS = this.tutorialService.getTutorialsByModuleId(moduleId);
+       return ResponseEntity.ok(tutorialDTOS);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<TutorialDTO> createTutorial(@Valid @RequestBody TutorialDTO tutorialDTO) {
+        TutorialDTO savedTutorialDTO = this.tutorialService.saveTutorial(tutorialDTO);
+        return new ResponseEntity<>(savedTutorialDTO, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/create-all")
+    public ResponseEntity<List<TutorialDTO>> createTutorials(@Valid @RequestBody List<TutorialDTO> tutorialDTOS) {
+        List<TutorialDTO> savedTutorialDTOs = this.tutorialService.saveTutorials(tutorialDTOS);
+        return new ResponseEntity<>(savedTutorialDTOs, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTutorialById(@PathVariable Integer id) {
+        this.tutorialService.deleteTutorialById(id);
+        return ResponseEntity.ok().build();
+    }
 }
